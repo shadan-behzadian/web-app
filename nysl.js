230038cf321e7e6ce7f5c9851e1bec0ed3b98412
shadan behzadian <shadan@shadans-MacBook-Pro.local>
2018-10-14 22:55:11 +0200

    fix

--- nysl.js
+++ nysl.js
@@ -2,9 +2,9 @@ var nysl = {
     gameInfo: [{
             "Date": "2018/07/10",
             "local": "Foxes",
-            "localimg":"u1.jpg",
+            "localimg":"img/u1.jpg",
             "visitor":"Horses",
-           "visitorimg":"u4.jpg",
+           "visitorimg":"img/u4.jpg",
             "location": "AJ Katzenmaier",
             "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.781052061583!2d-87.86492878466656!3d42.32586847918919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sA+J+Katzenmaier+Elementary+School!5e0!3m2!1sen!2ses!4v1530883294225",
             "time": "9:30 a.m."
@@ -12,9 +12,9 @@ var nysl = {
         {
             "Date": "2018/07/10",
               "local": "Penguins",
-            "localimg":"u3.jpg",
+            "localimg":"img/u3.jpg",
             "visitor":"Horses",
-            "visitorimg":"u4.jpg",
+            "visitorimg":"img/u4.jpg",
             "location": "Greenbay",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5858505787564!2d-87.66511018467999!3d41.92326097921863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2ses!4v1531815016993",
             "time": "1:00 p.m."
@@ -22,9 +22,9 @@ var nysl = {
         {
             "Date": "2018/09/08",
              "local": "Cats",
-               "localimg":"u5.jpg",
+               "localimg":"img/u5.jpg",
             "visitor":"Kangaroos",
-            "visitorimg":"u6.jpg",
+            "visitorimg":"img/u6.jpg",
             "location": "Howard A Yeager",
              "url":"https://www.google.com/maps/embed?pb",
             "time": "9:30 a.m."
@@ -32,9 +32,9 @@ var nysl = {
         {
             "Date": "2018/09/08",
               "local": "Kangaroos",
-             "localimg":"u6.jpg",
+             "localimg":"img/u6.jpg",
             "visitor":"Foxes",
-            "visitorimg":"u1.jpg",
+            "visitorimg":"img/u1.jpg",
             "location": "Marjorie P Hart",
              "url":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.7203115701636!2d-87.8478786846665!3d42.32716387918905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fecd0628e1bb5%3A0x9a4d5f9aafd42e02!2sMarjorie+P+Hart+Elementary+School!5e0!3m2!1sen!2ses!4v1530885067626" ,
             "time": "1:00 p.m."
@@ -43,9 +43,9 @@ var nysl = {
         {
             "Date": "2018/09/15",
               "local": "Owls",
-             "localimg":"u2.jpg",
+             "localimg":"img/u2.jpg",
             "visitor":"Horses",
-            "visitorimg":"u4.jpg",
+            "visitorimg":"img/u4.jpg",
             "location": "North",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11798.88131263691!2d-87.85444476403316!3d42.32716352540188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fecd3085aa3bd%3A0x54f2728b0f77d756!2sNorth+Chicago+Community+High+School!5e0!3m2!1sen!2ses!4v1530885173277",
             "time": "9:30 a.m."
@@ -54,9 +54,9 @@ var nysl = {
         {
             "Date": "2018/07/05",
               "local": "Penguins",
-             "localimg":"u3.jpg",
+             "localimg":"img/u3.jpg",
             "visitor":"Cats",
-            "visitorimg":"u5.jpg",
+            "visitorimg":"img/u5.jpg",
             "location": "AJ Katzenmaier",
              "url":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.781052061583!2d-87.86492878466656!3d42.32586847918919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sA+J+Katzenmaier+Elementary+School!5e0!3m2!1sen!2ses!4v1530883294225",
             "time": "1:00 p.m."
@@ -64,9 +64,9 @@ var nysl = {
         {
             "Date": "2018/09/22",
               "local": "Foxes",
-             "localimg":"u1.jpg",
+             "localimg":"img/u1.jpg",
             "visitor":"Penguins",
-            "visitorimg":"u3.jpg",
+            "visitorimg":"img/u3.jpg",
             "location": "South",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11798.88143754278!2d-87.85444476589377!3d42.32716285944942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccac2135a21%3A0x8ce88e90b96bbec1!2sSouth+Elementary+School!5e0!3m2!1sen!2ses!4v1530885267904",
             "time": "9:30 a.m."
@@ -75,9 +75,9 @@ var nysl = {
         {
             "Date": "2018/09/22",
               "local": "Owls",
-             "localimg":"u2.jpg",
+             "localimg":"img/u2.jpg",
             "visitor":"Kangaroos",
-            "visitorimg":"u6.jpg",
+            "visitorimg":"img/u6.jpg",
             "location": "Howard A Yeager",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5858505787564!2d-87.66511018467999!3d41.92326097921863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2ses!4v1531815016993"
                },
@@ -85,9 +85,9 @@ var nysl = {
         {
             "Date": "2018/09/29",
               "local": "Horses",
-             "localimg":"u4.jpg",
+             "localimg":"img/u4.jpg",
             "visitor":"Cats",
-            "visitorimg":"u5.jpg",
+            "visitorimg":"img/u5.jpg",
             "location": "Greenbay",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5858505787564!2d-87.66511018467999!3d41.92326097921863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2ses!4v1531815016993",
             "time": "9:30 a.m."
@@ -96,9 +96,9 @@ var nysl = {
         {
             "Date": "2018/10/06",
             "local": "Owls",
-             "localimg":"u2.jpg",
+             "localimg":"img/u2.jpg",
             "visitor":"Cats",
-            "visitorimg":"u5.jpg",
+            "visitorimg":"img/u5.jpg",
             "location": "Marjorie P Hart",
              "url": "https://www.google.com/maps/embed?pb", 
             "time": "9:30 a.m."
@@ -106,9 +106,9 @@ var nysl = {
         {
             "Date": "2018/10/06",
               "local": "Foxes",
-             "localimg":"u1.jpg",
+             "localimg":"img/u1.jpg",
             "visitor":"Kangaroos",
-            "visitorimg":"u6.jpg",
+            "visitorimg":"img/u6.jpg",
             "location": "South",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11798.88143754278!2d-87.85444476589377!3d42.32716285944942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880feccac2135a21%3A0x8ce88e90b96bbec1!2sSouth+Elementary+School!5e0!3m2!1sen!2ses!4v1530885267904",
             "time": "1:00 p.m."
@@ -116,9 +116,9 @@ var nysl = {
         {
             "Date": "2018/10/08",
               "local": "Penguins",
-             "localimg":"u3.jpg",
+             "localimg":"img/u3.jpg",
             "visitor":"Horses",
-            "visitorimg":"u4.jpg",
+            "visitorimg":"img/u4.jpg",
             "location": "Howard A Yeager",
              "url":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5858505787564!2d-87.66511018467999!3d41.92326097921863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2ses!4v1531815016993",
             "time": "9:30 a.m."
@@ -126,9 +126,9 @@ var nysl = {
         {
             "Date": "2018/10/08",
             "local": "Cats",
-             "localimg":"u5.jpg",
+             "localimg":"img/u5.jpg",
             "visitor":"Foxes",
-            "visitorimg":"u1.jpg",
+            "visitorimg":"img/u1.jpg",
             "location": "Greenbay",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d182055.91912350614!2d-88.13057850503957!3d44.5228598380538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8802e2e809b380f3%3A0x6370045214dcf571!2sGreen+Bay%2C+WI%2C+USA!5e0!3m2!1sen!2ses!4v1530884931682",
             "time": "1:00 p.m."
@@ -136,9 +136,9 @@ var nysl = {
         {
             "Date": "2018/10/20",
              "local": "Kangaroos",
-             "localimg":"u6.jpg",
+             "localimg":"img/u6.jpg",
             "visitor":"Penguins",
-            "visitorimg":"u3.jpg",
+            "visitorimg":"img/u3.jpg",
             "location": "North",
              "url": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11798.88131263691!2d-87.85444476403316!3d42.32716352540188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fecd3085aa3bd%3A0x54f2728b0f77d756!2sNorth+Chicago+Community+High+School!5e0!3m2!1sen!2ses!4v1530885173277",
             "time": "9:30 a.m."
@@ -146,9 +146,9 @@ var nysl = {
         {
             "Date": "2018/10/20",
              "local": "Owls",
-             "localimg":"u2.jpg",
+             "localimg":"img/u2.jpg",
             "visitor":"Horses",
-            "visitorimg":"u4.jpg",
+            "visitorimg":"img/u4.jpg",
             "location": "Marjorie P Hart",
              "url":"https://www.google.com/maps/embed?pb", 
             "time": "1:00 p.m."
@@ -159,7 +159,7 @@ var nysl = {
              "local": "Penguins",
              "localimg":"img/u3.jpg",
             "visitor":"Foxes",
-            "visitorimg":"u1.jpg",
+            "visitorimg":"img/u1.jpg",
             "location": "AJ Katzenmaier",
              "url":  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.781052061583!2d-87.86492878466656!3d42.32586847918919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880f932fbc6ba7cd%3A0xcf2bbe275c6da815!2sA+J+Katzenmaier+Elementary+School!5e0!3m2!1sen!2ses!4v1530883294225",
             "time": "9:30 a.m."
@@ -170,7 +170,7 @@ var nysl = {
              "local": "Cats",
              "localimg":"img/u5.jpg",
             "visitor":"Kangaroos",
-            "visitorimg":"u6.jpg",
+            "visitorimg":"img/u6.jpg",
             "location": "Howard A Yeager",
              "url":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2968.5858505787564!2d-87.66511018467999!3d41.92326097921863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2e37f852467%3A0xb6cb22b2f0358874!2sHoward+A+Yeager+Elementary!5e0!3m2!1sen!2ses!4v1531815016993" ,
             "time": "1:00 p.m."
@@ -180,11 +180,11 @@ var nysl = {
 
 ],
     teamInfo: [{
-            "image": "u1.jpg",
+            "image": "img/u1.jpg",
             "team": "Foxes",
 },
         {
-            "image": "u2.jpg",
+            "image": "img/u2.jpg",
             "team": "Owls"
 
 },
@@ -194,13 +194,13 @@ var nysl = {
       },
 
         {
-            "image": "u4.jpg",
+            "image": "img/u4.jpg",
             "team": "Horses"
 
         },
 
         {
-            "image": "u5.jpg",
+            "image": "img/u5.jpg",
             "team": "Cats"
 
       },
